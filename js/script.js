091daef2a45874423d513e64267c1bf034b56b09
MatheusSZR <matheusdesouzarodrigues34@gmail.com>
2024-11-23 16:57:58 -0300

    Adicionando

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+document.querySelectorAll('.zoom').forEach(item => {
+    item.addEventListener('click', function() {
+        this.style.transform = 'scale(1.5)';
+        setTimeout(() => {
+            this.style.transform = 'scale(1)';
+        }, 2000);
+    });
+});
